68deb3573c199b05035a9a0b57a750779aa1b18e
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-01-07 20:38:02 -0300

    bump dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Formulario from './componentes/Formulario';
 
 function App() {
-  return <h1>Olá!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Formulario />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
