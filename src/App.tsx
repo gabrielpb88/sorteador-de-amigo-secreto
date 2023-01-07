3c26f56080668d8fe77a9d0350a976cb178c90a2
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-01-07 20:46:19 -0300

    adicionado rota padrao

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Formulario from './componentes/Formulario';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Formulario />} />
-      </Routes>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={<Formulario />} />
+        </Routes>
+      </RecoilRoot>
     </Router>
   );
 }
